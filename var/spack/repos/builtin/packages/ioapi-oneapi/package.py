24321fdcd4526e08f51ec3e2d4bd25558e780d3c
Huy Tran <huy@cmascenter.org>
2024-06-20 16:14:29 -0400

    Added packge configurations for ioapi-oneapi, ioapi-large and smoke-oneapi; all to be compiled with Intel OneApi compiler

---
+++ var/spack/repos/builtin/packages/ioapi-oneapi/package.py
@@ -0,0 +1,135 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os, shutil
+
+from spack.package import *
+
+
+class IoapiOneapi(MakefilePackage):
+    """Models-3/EDSS Input/Output Applications Programming Interface."""
+
+    homepage = "https://www.cmascenter.org/ioapi/"
+
+#   url = "https://www.cmascenter.org/ioapi/download/ioapi-3.2.tar.gz"
+    # This checksum is somewhat meaningless because upstream updates the tarball
+    # without incrementing the version despite requests no to do this.
+    # Therefore the checksum fails everytime upstream silently updates the
+    # source tarball (#28247).  This also means that one must test for breaking
+    # changes when updating the checksum and avoid #22633.
+#   version("3.2", sha256="6c2c180f9ba3b3954dbc9ed9269dd028489156a07595d4d0b84c2723371b21f7")
+    git = "https://github.com/cjcoats/ioapi-3.2"
+    version("3.2", branch="master")
+
+    maintainers("hnqtran")
+
+    depends_on("intel-oneapi-compilers")
+    depends_on("hdf5@1.14.3 %oneapi -shared -mpi +cxx +fortran +hl ^szip ^zlib")
+    depends_on("netcdf-c@4.9.2 %oneapi -mpi -shared -dap ^hdf5")
+    depends_on("netcdf-fortran@4.6.1 %oneapi -shared ^netcdf-c")
+    depends_on("sed", type="build")
+
+    def edit(self, spec, prefix):
+        
+        # No default Makefile bundled; edit the template.
+        os.symlink("Makefile.template", "Makefile")
+
+        # Determine temporary source directory
+        temp_source_dir = self.stage.source_path
+
+        # Set Binary target
+        BIN = 'Linux2_x86_64ifx'
+
+        # The makefile uses stubborn assignments of = instead of ?= so
+        # edit the makefile instead of using environmental variables.
+        makefile = FileFilter("Makefile")
+
+        # Replace the specific commented line with the desired uncommented line
+        makefile.filter(r'^#\s*(BIN\s*=\s*Linux2_x86_64)',         'BIN     = {0}'.format(BIN))
+        makefile.filter(r'^#\s*(BASEDIR\s*=\s*\${PWD})', r'BASEDIR = {0}'.format(temp_source_dir))
+        makefile.filter(r'^#\s*(INSTALL\s*=\s*\${HOME})',          'INSTALL = {0}'.format(prefix))
+        makefile.filter(r'^#\s*(LIBINST\s*=\s*\$\(INSTALL\)/\$\(BIN\))', r'\1')
+        makefile.filter(r'^#\s*(BININST\s*=\s*\$\(INSTALL\)/\$\(BIN\))', r'\1')
+        makefile.filter(r'^#\s*(CPLMODE\s*=\s*nocpl.*)', r'\1')#CPLMODE   = pncf')
+        makefile.filter(r'^\s*(NCFLIBS\s*=\s*\-lnetcdff -lnetcdf)', r'#\1')
+        makefile.filter(r'^#\s*(NCFLIBS\s*=\s*\-lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lz)',r'NCFLIBS = -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lz -lm')
+        makefile.filter(r'^#\s*(IOAPIDEFS\s*=\s*\"-DIOAPI_NCF4")',  r'\1')
+        makefile.filter(r'^#\s*(PVMINCL\s*=\s*)(?!.*\$\(PVM_ROOT\)/conf/\$\(PVM_ARCH\).def)', r'\1')
+        makefile.filter(r'^\s*(BASEDIR\s*=\s*\${PWD})', r'#\1')
+
+        # Change the specific line to comment out ${IODIR}/Makefile and ${TOOLDIR}/Makefile
+        # Fix circular dependency bug for generating subdirectory Makefiles.
+        makefile.filter("^configure:.*", "configure:")
+
+        # Modify ioapi/Make.$(CPLMODE).sed file
+        makesed = FileFilter(os.path.join(temp_source_dir,'ioapi','Makefile.nocpl.sed'))
+        makesed.filter(r'^\s*(BASEDIR\s*=\s*\${HOME}/ioapi-3.2)',r'BASEDIR = {0}'.format(temp_source_dir))
+        makesed.filter(r'^\s*MAKEINCLUDE.\$\(BIN\)', r'MAKEINCLUDE.{0}'.format(BIN))
+        makesed.filter(r'^#\s*(DEFINEFLAGS\s*=\s*\-DIOAPI_NCF4=1\s*\$\(ARCHFLAGS\)\s*\$\(PARFLAGS\))', r'\1') # Uncomment
+        makesed.filter(r'^\s*(DEFINEFLAGS\s*=\s*\-DIOAPI_PNCF=1\s*\$\(ARCHFLAGS\)\s*\$\(PARFLAGS\))', r'#\1') # Comment out this line
+        makesed.filter(r'^#(VFLAG\s*=\s*-DVERSION=\'3\.2-nocpl-ncf4\')', r'\1')
+        makesed.filter(r'^\s*(VFLAG\s*=\s*\-DVERSION=\'3\.2-nocpl-mpi\')', r'#\1')
+
+        # Modify m3tool/Make.$(CPLMODE).sed file
+        m3toolsed = FileFilter(os.path.join(temp_source_dir,'m3tools','Makefile.nocpl.sed'))
+        m3toolsed.filter(r'^\s*(BASEDIR\s*=\s*\${HOME}/ioapi-3.2)',r'BASEDIR = {0}'.format(temp_source_dir))
+
+        # Modify ioapi/Makeinclude file
+        shutil.copy("ioapi/Makeinclude.Linux2_x86_64ifort","ioapi/Makeinclude.{0}".format(BIN))
+        makeinc = FileFilter(os.path.join(temp_source_dir,'ioapi',f'Makeinclude.{BIN}'))
+        makeinc.filter(r'icc', r'icx')
+        makeinc.filter(r'icpc', r'icpx')
+        makeinc.filter(r'^\s*FC\s*=\s*ifort.*',r'FC = ifx  -auto -warn notruncated_source -Bstatic -static-intel')
+        makeinc.filter(r'\s*(OMPFLAGS\s*=\s*\-openmp.*)',r'OMPFLAGS  = -qopenmp')
+        makeinc.filter(r'\s*(OMPLIBS\s*=\s*\-openmp.*)',r'OMPLIBS   = -qopenmp -qopenmp-link=static -static-intel')
+        makeinc.filter(r'#\s*(MFLAGS\s*=\s*\-traceback\s*# generic)',r'\1')
+        makeinc.filter(r'\s*(MFLAGS\s*=\s*\-traceback -xHost \s*# this-machine)',r'#\1')
+        makeinc.filter(r'-stack_temps', '-stack-temps')
+        makeinc.filter(r'-safe_cray_ptr', '-safe-cray-ptr')
+
+       # Modify ioapi/sortic.c
+        with open(os.path.join(temp_source_dir,'ioapi','sortic.c'), 'r') as f:
+            lines = f.readlines()
+
+        # Find the line with #include "parms3.h"
+        for i, line in enumerate(lines):
+            if line.strip().startswith('#include "parms3.h"'):
+                # Insert #include <stdlib.h> after #include "parms3.h"
+                lines.insert(i + 1, '#include <stdlib.h>\n')
+                break
+
+        # Write the modified lines back 
+        with open(os.path.join(temp_source_dir,'ioapi','sortic.c'), 'w') as f:
+            f.writelines(lines)
+
+        # Generate the subdirectory Makefiles.
+        make("configure")
+        make("dirs")
+        make("fix")
+
+    def install(self, spec, prefix):
+        make("install")
+
+        # Install the header files for downstream installation of SMOKE and CMAQ.
+        mkdirp(prefix.ioapi.fixed_src)
+        install("ioapi/*.EXT", prefix.ioapi)
+        install("ioapi/Makeinclude.Linux2_x86_64ifx", prefix.ioapi)
+        install("ioapi/*.EXT", prefix.ioapi)
+        install("ioapi/fixed_src/*.EXT", prefix.ioapi.fixed_src)
+
+        # Path to the source directory you want to copy
+#       source_dir = os.path.join(self.stage.source_path,"ioapi")
+
+        # Path to the destination directory (installation prefix)
+#       destination_dir = os.path.join(prefix, 'ioapi')
+
+        # Create the destination directory if it doesn't exist
+#       os.makedirs(destination_dir, exist_ok=True)
+#       try:
+            # Copy the entire source directory to the destination directory
+#           shutil.copytree(source_dir, destination_dir)
+#           print(f"Copied {source_dir} to {destination_dir}")
+#       except OSError as e:
+#           raise RuntimeError(f"Error copying directory: {e}")
---
+++ var/spack/repos/builtin/packages/smoke-oneapi/package.py
@@ -0,0 +1,83 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os, shutil, subprocess
+
+from spack.package import *
+
+
+class SmokeOneapi(MakefilePackage):
+    """The Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling System"""
+
+    homepage = "https://github.com/CEMPD/SMOKE/releases/tag/SMOKEv5_Jun2023"
+    git = "https://github.com/CEMPD/SMOKE"
+    version("5.0", branch="master")
+
+    maintainers("hnqtran")
+
+    depends_on("intel-oneapi-compilers")
+    depends_on("ioapi-oneapi ^oneapi")
+
+    def edit(self, spec, prefix):
+        
+        # Determine temporary source directory
+        temp_source_dir = self.stage.source_path
+
+        # Determine path to ioapi
+        ioapi_dir = self.spec['ioapi-oneapi'].prefix
+
+        # Determine linked library in netcdf
+        # Execute the command `nc-config --libs` using subprocess
+        try:
+            result = subprocess.run(['nc-config', '--libs'], stdout=subprocess.PIPE, check=True)
+            # Decode the output bytes to string assuming utf-8 encoding
+            nclibs = result.stdout.decode('utf-8').strip()
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError(f"Error running nc-config --libs: {e}")
+
+        # Set Binary target
+        BIN = 'Linux2_x86_64ifx'
+
+        os.symlink("src/Makefile", "Makefile")
+        makefile = FileFilter(os.path.join(temp_source_dir,'src','Makefile'))
+        makefile.filter(r'^\s*(include\s*Makeinclude)',f'include {temp_source_dir}/src/Makeinclude')
+
+        makeincl = FileFilter(os.path.join(temp_source_dir,'src','Makeinclude'))
+
+        # Replace the specific commented line with the desired uncommented line
+        makeincl.filter(r'^\s*(BASEDIR\s*=\s*\${SMK_HOME}/subsys/smoke/src)','BASEDIR = {0}/src'.format(temp_source_dir))
+        makeincl.filter(r'^\s*IOBASE\s*=.*','IOBASE = {0}'.format(ioapi_dir))
+        makeincl.filter(r'^\s*(OBJDIR\s*=\s*\${BASEDIR}/../\${BIN})',r'OBJDIR  = {0}/{1}'.format(temp_source_dir,BIN))
+        makeincl.filter(r'^\s*(IOBIN\s*=\s*\${IOBASE}.*)','IOBIN  = {0}/{1}'.format(ioapi_dir,BIN))
+        makeincl.filter(r'^\s*INSTDIR\s*=\s*/somewhere.*','INSTDIR = {0}/{1}'.format(prefix,BIN))
+
+        makeincl.filter(r'^\s*(include\s*\${IODIR}/Makeinclude.\${BIN})','include {0}/ioapi/Makeinclude.{1}'.format(ioapi_dir,BIN))
+
+        makeincl.filter(r'^\s*(IFLAGS\s*=\s*\-I${IOINC} -I${INCDIR} -I${IOBIN}.*)', r'IFLAGS = -I${IOINC} -I${INCDIR} -I${IOBIN} -traceback')
+        makeincl.filter(r'^\s*(EFLAG\s*=\s*\-extend-source 132 -zero.*)',r'EFLAG = -extend-source 132 -zero -static-intel -debug')
+        makeincl.filter(r'^\s*(IOLIB\s*=\s*\-L\$\(IOBIN\) -lioapi -lnetcdff -lnetcdf)','IOLIB = -L$(IOBIN) -lioapi -lnetcdff {0}'.format(nclibs))
+
+        makefile.filter("^install:.*", "install:")
+
+        make("dir")
+
+    def install(self, spec, prefix):
+        mkdirp(prefix.Linux2_x86_64ifx)
+        make("lib")
+        make("exe")
+        make('install')
+#       make('all')
+
+#       # Ensure the Makefile is present in the correct subfolder
+#       makefile_dir = os.path.join(self.stage.source_path, 'src')
+
+        # Check if the Makefile exists in the specified directory
+#       if not os.path.exists(os.path.join(makefile_dir, 'Makefile')):
+#           raise RuntimeError(f"Makefile not found in {makefile_dir}")
+
+        # Change directory to the subfolder containing the Makefile
+#       with working_dir(makefile_dir):
+            # Execute make install command
+#           make('all', parallel=False)  # Adjust parallelism as needed
